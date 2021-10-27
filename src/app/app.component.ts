3275902816bb6e839ee25056d7e626cb03eed633
Sazonova Anna <anna_sazonova2@mail.ru>
2021-10-27 17:50:01 +0300

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,24 +12,24 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
   title = '';
   stickers: any[] = []
-  now:any;
+  now: any;
   text = "";
-  @ViewChild('TextInput') textInput!: ElementRef;
   idCounter = 0;
-  deleteSticker(id:any){
-    console.log(id);
-    for(let i = 0;i<this.stickers.length;i++){
-      if(this.stickers[i].id == id){
+  deleteSticker(id: any) {
+    for (let i = 0; i < this.stickers.length; i++) {
+      if (this.stickers[i].id == id) {
         this.stickers.splice(i, 1)
       }
     }
   }
-  addNote(title:string,text:string){
-    let buf = title.split("")
-    buf[0] = buf[0].toUpperCase()
-    title = buf.join("")
+  addNote(title: string, text: string) {
     this.now = Date.now();
-    this.stickers.push( { title: title, text: text , id : ++this.idCounter, date: this.now})
+    this.stickers.push({
+      title: title,
+      text: text,
+      id: ++this.idCounter,
+      date: this.now
+    })
     this.title = ""
     this.text = ""
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { ElementRef, NgModule, ViewChild } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
